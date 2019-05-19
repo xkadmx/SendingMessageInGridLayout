8d74d7ee9de5fed6bb1b26fdcc2b11dfcf4180fc
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-19 15:22:43 +0200

    adding onClick parameter to the button and setting boolean .isChecked() to indicate whether button was clicked (on) or not and display it on ToggleButton

--- app/src/main/java/com/example/sendingmessageingridlayout/MainActivity.java
+++ app/src/main/java/com/example/sendingmessageingridlayout/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sendingmessageingridlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onToggleButtonClicked(View view) {
+        boolean on = ((ToggleButton) view).isChecked();
+        if(on){
+        }else{
+
+        }
     }
 }
